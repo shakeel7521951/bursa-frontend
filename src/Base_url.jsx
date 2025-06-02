2c850428208c9532c59c2b844b7ddcba3abde554
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-06-02 19:22:18 +0500

    change base url

--- src/Base_url.jsx
+++ src/Base_url.jsx
@@ -1,4 +1,4 @@
-// const BASE_URL = "https://bursa-backend-1.onrender.com/api/v1";
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://bursa-backend-1.onrender.com/api/v1";
+// const BASE_URL = "http://localhost:5000/api/v1";
 
 export default BASE_URL;
